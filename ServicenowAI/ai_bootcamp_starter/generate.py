e8b909d3e370b90d3bac8b93cc7de788db0002ba
24CSB1A28Susheelkumar <ks24csb1a28@student.nitw.ac.in>
2026-01-04 11:39:25 +0530

    Update generate.py

--- ServicenowAI/ai_bootcamp_starter/generate.py
+++ ServicenowAI/ai_bootcamp_starter/generate.py
@@ -18,8 +18,8 @@ class GenerateEmail:
     def __init__(self, model: str):
         self.client = AzureOpenAI(
             azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-            api_key=os.getenv("AZURE_OPENAI_KEY"),
-            api_version="2024-02-01"
+            api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+            api_version=os.getenv("AZURE_OPENAI_API_VERSION")
         )
         self.model = model
 
@@ -82,5 +82,6 @@ class GenerateEmail:
 
         raw_output = self._call_api(messages)
         return self._clean_body(raw_output)
+
 
 
